d313361639c705f3f0ce5c9505a30f64d148d7ce
Gabriel Aquino <gabriel.aquino.069@ufrn.edu.br>
2021-10-23 11:43:39 -0300

    Versão Inicial do Programa (1.0)

---
+++ Versão 1.0/Projeto Clínica Dentária 1 - 15.c
@@ -0,0 +1,107 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Serido                     ///
+///               Departamento de Computacao e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programacao                      ///
+///                  Projeto de Agendamento de Clinica Dentaria             ///
+///                Developed by  @gbrl08snts - Out, 2021                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+/////
+// Assinatura das funções
+void telaSobre(void);
+void telaPrincipal(void);
+void telaEquipe(void);
+
+/////
+// Programa principal
+int main(void) {
+    telaSobre();
+    telaPrincipal();
+    telaEquipe();
+    return 0;
+}
+
+/////
+// Funções
+
+void telaSobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                    ///\n");
+    printf("///               Departamento de Computação e Tecnologia                 ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                    ///\n");
+    printf("///                  Projeto de Agendamento de Clínica Dentária           ///\n");
+    printf("///                Developed by  @gbrl08snts - Out, 2021                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Sistema de Agendamento Odontológico = = = = =     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Programa da disciplina DCT1106 - Programação para fins didáticos    ///\n");
+    printf("///  e avaliativos. O programa contém os módulos principais e funções   ///\n");
+    printf("///  que serão exigidos na avaliação.                                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+
+void telaPrincipal(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                    ///\n");
+    printf("///               Departamento de Computação e Tecnologia                 ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                    ///\n");
+    printf("///                  Projeto Sistema de Gestão Escolar                     ///\n");
+    printf("///                Developed by  @gbrl08snts - Out, 2021                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Sistema de Agendamento Odontológico = = = = =     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Módulo Data                                              ///\n");
+    printf("///            2. Módulo Horário                                          ///\n");
+    printf("///            3. Módulo Dentista                                          ///\n");
+    printf("///            4. Módulo Pasciente                                         ///\n");
+    printf("///            5. Módulo Relatórios                                       ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+
+void telaEquipe(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                    ///\n");
+    printf("///               Departamento de Computação e Tecnologia                 ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                    ///\n");
+    printf("///                  Projeto Sistema de Agendamento Odontológico           ///\n");
+    printf("///                Developed by  @gbrl08snts - Out, 2021                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///         = = = = = Sistema de Agendamento Odontológico = = = = =        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///          Este projeto exemplo foi desenvolvido por:                     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///          Aluno: Gabriel José de Aquino Santos                          ///\n");
+    printf("///          E-mail: gabriel.aquino.069@ufrn.br                             ///\n");
+    printf("///          Redes sociais: @gbrl08snts                                     ///\n");
+    printf("///          Repositório: https://github.com/gbrl08snts?tab=repositories   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
